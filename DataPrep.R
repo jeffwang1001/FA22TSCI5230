5293f9f0edf7521d13176a13b24107597af5a09b
Liujie Wang <liujiewang@liujies-mbp.med.uthscsa.edu>
2022-09-07 15:35:52 -0500

    load and ggplot the data

---
+++ DataPrep.R
@@ -0,0 +1,56 @@
+#'---
+#' title: "TSCI 5230: Introduction to Data Science"
+#' author: 'Jeff Wang'
+#' abstract: |
+#'  | Provide a summary of objectives, study design, setting, participants,
+#'  | sample size, predictors, outcome, statistical analysis, results,
+#'  | and conclusions.
+#' documentclass: article
+#' description: 'Manuscript'
+#' clean: false
+#' self_contained: true
+#' number_sections: false
+#' keep_md: true
+#' fig_caption: true
+#' output:
+#'  html_document:
+#'    toc: true
+#'    toc_float: true
+#'    code_folding: show
+#' ---
+#'
+#+ init, echo=FALSE, message=FALSE, warning=FALSE
+# init ----
+# This part does not show up in your rendered report, only in the script,
+# because we are using regular comments instead of #' comments
+debug <- 0;
+knitr::opts_chunk$set(echo=debug>-1, warning=debug>0, message=debug>0);
+
+library(ggplot2); # visualisation
+library(GGally);
+#library(rio);# simple command for importing and exporting
+library(pander); # format tables
+library(printr); # set limit on number of lines printed
+library(broom); # allows to give clean dataset
+library(dplyr); #add dplyr library
+
+options(max.print=42);
+panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf);
+
+# load data ----
+
+if(!file.exists("working_script.rdata")){
+  system("R -f data.R")
+}
+
+load("working_script.rdata")
+
+View(patients)
+
+ggplot(data = patients, aes(x = anchor_age, fill = gender))+
+  geom_histogram() +
+  geom_vline(xintercept = 65)
+
+table(patients$gender)
+#check for duplicates in the subject_id
+
